f9526f6fa01f4a082a6981862778d2ccc7dcdd5f
iamtrungnhan <doantrungnhan24@gmail.com>
2024-09-26 16:13:12 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/home', function(){
-    return  'hello word';
-});
-
-Route::get('/hhome', function(){
-    return  'hello word';
-});
