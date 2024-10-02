9ae9df7d46d4177ec3bd73a23d78a455b86c99b8
dihxrid <hoangdang3210@gmail.com>
2024-10-02 17:16:04 +0700

    Tag
    CRUD Tag

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-<<<<<<< Updated upstream
-Route::get('/', function () {
-    return view('welcome');
-});
-=======
 require __DIR__ . '/admin.php';
 require __DIR__ . '/tag.php';
 
 Route::get('/', function () {
     return view('welcome');
 });
->>>>>>> Stashed changes
+
